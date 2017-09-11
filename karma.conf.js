6fcf4542b898570acaec7cc45043bacf9f10dbda
Benjamin Ikeji <benjaminikeji@gmail.com>
2017-09-11 12:02:47 +0100

    Updated karma.conf.js Travis auto publish config

--- karma.conf.js
+++ karma.conf.js
@@ -60,47 +60,33 @@ module.exports = function (config) {
 
 
 		// web server port
-		port
-:
-	9876,
+		port: 9876,
 
 
 		// enable / disable colors in the output (reporters and logs)
-		colors
-:
-	true,
+		colors: true,
 
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel
-:
-	config.LOG_INFO,
+		logLevel: config.LOG_INFO,
 
 
 		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch
-:
-	true,
+		autoWatch: true,
 
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers
-:
-	['Chrome'],
+		browsers: ['Chrome'],
 
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
-		singleRun
-:
-	false,
+		singleRun: false,
 
 		// Concurrency level
 		// how many browser should be started simultaneous
-		concurrency
-:
-	Infinity
-})
+		concurrency: Infinity
+	})
 };
